cfa92e33853d532b0464ee5a581ac3a29290e841
anakon <nazarkinah73@gmail.com>
2024-12-10 20:00:42 +0000

    feat(routes): add routes for services api

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,17 @@
 <?php
 
 use App\Http\Controllers\MeetingController;
+use App\Http\Controllers\ServiceController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/meetings')->controller(MeetingController::class)->group(function () {
   Route::get('/', 'index');
   Route::get('/date/{date}', 'getByDate');
   Route::get('/{meeting}', 'show');
+});
+
+Route::prefix('/services')->controller(ServiceController::class)->group(function () {
+  Route::get('/', 'index');
+  Route::get('/date/{date}', 'getByDate');
+  Route::get('/{service}', 'show');
 });
