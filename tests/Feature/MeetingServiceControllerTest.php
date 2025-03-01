a5060a2c1b89114b1194963e0df744d8b7092b9a
anakon <nazarkinah73@gmail.com>
2025-03-01 22:56:15 +0000

    test(MeetingServiceControllerTest): test for update method

--- tests/Feature/MeetingServiceControllerTest.php
+++ tests/Feature/MeetingServiceControllerTest.php
@@ -4,6 +4,7 @@ use App\Models\Meeting;
 use App\Models\Service;
 use Database\Seeders\DatabaseSeeder;
 
+use function Pest\Laravel\patchJson;
 use function Pest\Laravel\postJson;
 
 beforeEach(function () {
@@ -63,4 +64,60 @@ test('MeetingServiceController creates Service and Meeting with shared date', fu
 
   expect(Meeting::where('leading', 'Alice Smith')->where('date', '2025-02-20')->exists())->toBeTrue();
   expect(Service::where('scene', 'Main Hall')->where('date', '2025-02-20')->exists())->toBeTrue();
+});
+
+test('MeetingServiceController updates Service and Meeting with shared date', function () {
+  $payload = [
+    'date' => '2025-02-20',
+
+    'service' => [
+      'scene' => 'Main Hall2',
+      'microphones' => '2',
+      'voiceover_zoom' => 'Enabled',
+      'administrator' => 'John Doe',
+    ],
+
+    'meeting' => [
+      'leading' => 'Alice Smith2',
+      'speaker' => 'Bob Johnson',
+      'speech_title' => 'The Future of AI',
+      'lead_wt' => 'Jane Doe',
+      'reader' => 'Michael Brown',
+      'closing_prayer' => 'Emily White',
+      'special_program' => 'Panel Discussion',
+      'status_id' => 1,
+      'address_id' => 1,
+      'ministry_meeting_id' => 1,
+    ]
+  ];
+
+  $firstId = Meeting::min('id');
+
+  $response = patchJson("/api/meeting-service/{$firstId}", $payload);
+
+  $response->assertStatus(200)
+    ->assertJsonStructure([
+      'message',
+      'data' => [
+        'service' => ['id', 'date', 'scene', 'microphones', 'voiceover_zoom', 'administrator'],
+        'meeting' => [
+          'id',
+          'date',
+          'leading',
+          'speaker',
+          'speech_title',
+          'lead_wt',
+          'reader',
+          'closing_prayer',
+          'special_program',
+          'status_id',
+          'service_id',
+          'address_id',
+          'ministry_meeting_id'
+        ],
+      ],
+    ]);
+
+  expect(Meeting::where('leading', 'Alice Smith2')->where('date', '2025-02-20')->exists())->toBeTrue();
+  expect(Service::where('scene', 'Main Hall2')->where('date', '2025-02-20')->exists())->toBeTrue();
 });
