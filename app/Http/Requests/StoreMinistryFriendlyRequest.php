ed6e63fbb669e0efd70f464b203a49fa65a6708e
anakon <nazarkinah73@gmail.com>
2025-03-01 22:55:13 +0000

    feat(MinistryFriendlyController): impl

---
+++ app/Http/Requests/StoreMinistryFriendlyRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreMinistryFriendlyRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'with_friendly' => 'sometimes|boolean',
+
+            'ministry.date' => 'required|date',
+            'ministry.leader' => 'nullable|string',
+            'ministry.address' => 'nullable|string',
+            'ministry.address_url' => 'nullable|url',
+
+            'friendly.date' => 'required_if:with_friendly,true|date',
+            'friendly.address' => 'required_if:with_friendly,true|string',
+            'friendly.address_url' => 'required_if:with_friendly,true|url',
+            'friendly.description' => 'required_if:with_friendly,true|string',
+            'friendly.inviting' => 'required_if:with_friendly,true|string',
+        ];
+    }
+}
